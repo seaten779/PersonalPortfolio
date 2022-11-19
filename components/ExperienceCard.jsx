fde1f18f412c42930521dc98d0c9bbb74ba2b6fe
Suyog Chaulagain <117005561+seaten779@users.noreply.github.com>
2022-11-19 11:10:02 -0500

    debug

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -8,57 +8,14 @@ import {
 	CardText,
 	Col,
 } from "reactstrap";
-
+import innercomponent from "./innercomponent";
 import { Fade } from "react-reveal";
 
 const ExperienceCard = ({ data }) => {
 	return (
 		<Col lg="6">
-			<Fade left duration={2000}>
-				<Card
-					style={{ flex: 1 }}
-					className="shadow-lg--hover mb-3 shadow border-0 text-center rounded"
-				>
-					<CardBody className="">
-						<img
-							src={data.companylogo}
-							style={{
-								objectFit: "cover",
-								left: 0,
-								right: 0,
-								top: "7rem",
-								marginLeft: "auto",
-								marginRight: "auto",
-								width: "8rem",
-								height: "8rem",
-								borderRadius: "50%",
-							}}
-							className="shadow mb-3"
-							alt={data.companylogo}
-						/>
-						<CardTitle tag="h4" className="mb-2">
-							{data.company}
-						</CardTitle>
-						<CardSubtitle tag="h5" className="mb-2">
-							{data.role}
-						</CardSubtitle>
-						<CardSubtitle>{data.date}</CardSubtitle>
-						<CardText
-							tag="div"
-							className="description my-3 text-left"
-						>
-							{data.desc}
-							<ul>
-								{data.descBullets
-									? data.descBullets.map((desc) => {
-											return <li key={desc}>{desc}</li>;
-									  })
-									: null}
-							</ul>
-						</CardText>
-					</CardBody>
-				</Card>
-			</Fade>
+			{data.link? <a href={data.link} > <innercomponent/>  </a> :<innercomponent/>}  
+			
 		</Col>
 	);
 };
